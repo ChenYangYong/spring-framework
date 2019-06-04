cecde4e38f453966f37aab617dce285fa7061cb3
yangyong.chen@foxmail.com <yangyong.chen@foxmail.com>
2019-06-04 17:54:40 +0800

    别名测试代码提交

--- spring-beans/src/test/java/yancy/XmlBeanFactoryTest.java
+++ spring-beans/src/test/java/yancy/XmlBeanFactoryTest.java
@@ -1,14 +1,20 @@
 package yancy;
 
+import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StringUtils;
 
 public class XmlBeanFactoryTest {
 	public static void main(String[] args) {
 		BeanFactory bf= new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
 		BeanTest bean = (BeanTest)bf.getBean("test");
 		System.out.println(bean);
+		BeanTest bean1 = (BeanTest)bf.getBean("test1");
+		System.out.println(bean1);
+		String[] aliases = bf.getAliases("test1");
+		System.out.println(Arrays.asList(aliases));
 		Car car = (Car)bf.getBean("car");
 		System.out.println(car);
 		Object obj = bf.getBean("&car");
