f93c4d411233129ab72e3b855a2745ce9985e998
yangyong.chen@foxmail.com <yangyong.chen@foxmail.com>
2019-06-12 21:52:48 +0800

    测试bean循环依赖

---
+++ spring-beans/src/test/java/yancy/circularreference/TestC.java
@@ -0,0 +1,17 @@
+package yancy.circularreference;
+
+public class TestC {
+	private TestA testA;
+
+	public TestC(TestA testA) {
+		this.testA = testA;
+	}
+
+	public TestA getTestA() {
+		return testA;
+	}
+
+	public void setTestA(TestA testA) {
+		this.testA = testA;
+	}
+}
