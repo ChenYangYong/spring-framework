d3345250d0cf02c104016326f5f43b85335e0dbb
yangyong.chen@foxmail.com <yangyong.chen@foxmail.com>
2020-01-03 21:52:38 +0800

    测试文件提交

---
+++ spring-test/src/test/resources/test.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `age` int(4) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `sex` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
